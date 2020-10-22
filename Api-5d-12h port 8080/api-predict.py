fe5b01846a13c424414a5b3d0e791e377b330489
Tran Duc Duy <41152040+Tendo-Jukkudo@users.noreply.github.com>
2020-10-22 19:13:21 +0700

    update save json

--- Api-5d-12h port 8080/api-predict.py
+++ Api-5d-12h port 8080/api-predict.py
@@ -4,8 +4,6 @@ import json
 import numpy as np
 import time 
 
-data_json = {'status':'','data_predict':[]}
-
 path_model_flow = 'model/model_flow.h5'
 path_model_p = 'model/model_p.h5'
 url_get = 'http://hbqweblog.com/DAKWACO/stock/ai_get_data.php?dev_id=F98420'
@@ -23,15 +21,31 @@ app = Flask(__name__)
 def returnPredict():
     run = request.args.get('run', default = 0, type = int)
     if(run == 1):
-        forecast_flow = model_predict.run_prediction(type_feature=1,nb_past=past_history,nb_future=future_target,path_model=path_model_flow,url_get=url_get,mean_std=True)
-        forecast_p = model_predict.run_prediction(type_feature=0,nb_past=past_history,nb_future=future_target,path_model=path_model_p,url_get=url_get,mean_std=True)
-        json_dump = json.dumps({'forecast-flow':forecast_flow,
-                                'forecast-Pressure':forecast_p},cls=NumpyEncoder)
-        data_json['data_predict'] = json_dump
+        in_file = open("data_predict.json", "r")
+        data = json.load(in_file) 
+        data = json.loads(data)
+        flow = data['flow']
+        Pressure = data['Pressure']
+        forecast_flow,error_date = model_predict.run_prediction(type_feature=1,
+                                                                nb_past=past_history,nb_future=future_target,
+                                                                path_model=path_model_flow,url_get=url_get,mean_std=True)
+        forecast_p,error_date = model_predict.run_prediction(type_feature=0,
+                                                             nb_past=past_history,nb_future=future_target,
+                                                             path_model=path_model_p,url_get=url_get,mean_std=True)
+        if forecast_flow[len(forecast_flow)-1][0] != flow[len(flow)-1][0]:
+            for i in forecast_flow:
+                flow.append(i)
+            for i in forecast_p:
+                Pressure.append(i)
         named_tuple = time.localtime() 
         time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
-        data_json['status'] = time_string   
-    return jsonify({'output':data_json})
+        data_out = json.dumps({'flow':flow,'Pressure':Pressure,'status':time_string,'error':error_date},cls=NumpyEncoder) 
+        out_file = open("data_predict.json", "w")
+        json.dump(data_out, out_file, indent = 6)    
+    else:
+        in_file = open("data_predict.json", "r")
+        data_out = json.load(in_file) 
+    return jsonify({'output':data_out})
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+    app.run(debug=True,host='0.0.0.0',port=8080)
